7e9f9cb445403ae15c29a993e0bd06a8c1153a3f
octane\ola <ola_midor@outlook.com>
2020-11-15 20:38:54 +0100

    db seeding script added

---
+++ seed-db/import.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+// see https://www.npmjs.com/package/mongo-seeding
+process.env.DEBUG = 'mongo-seeding';
+const { Seeder } = require('mongo-seeding');
+const path = require('path');
+
+const envFiles = {
+  development: '.env',
+  test: '.env.test',
+};
+
+// eslint-disable-next-line import/no-unresolved
+require('dotenv').config({ path: envFiles[process.env.NODE_ENV] });
+
+const config = {
+  database: process.env.DB_CONNECTION,
+  // dropDatabase: false,
+  // dropCollections: true,
+};
+
+const seeder = new Seeder(config);
+// see https://github.com/pkosiec/mongo-seeding/blob/master/docs/import-data-definition.md
+const collections = seeder.readCollectionsFromPath(
+  path.resolve('./seed-db/data')
+);
+
+// eslint-disable-next-line no-return-await
+(async () => {
+  try {
+    await seeder.import(collections);
+    console.log('data has been successfully imported');
+  } catch (e) {
+    console.log(e);
+  }
+})();
