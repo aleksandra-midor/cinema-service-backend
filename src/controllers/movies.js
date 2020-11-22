7632280e6c7c4a5a11c6908f12fb59e4d58536cf
octane\ola <ola_midor@outlook.com>
2020-11-22 17:06:20 +0100

    movies: endpoint for movies based on cinema id WIP updated seed for cinemas

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -14,4 +14,37 @@ router.get('/', async (req, res) => {
   }
 });
 
+// get movies for the given cinema by cinema id
+// router.get('/:cinemaId', async (req, res) => {
+//   console.log('-------------------------');
+//   try {
+//     const foundMovies = [];
+//     const foundCinema = await Cinema.findById(req.params.cinemaId);
+//     foundCinema.repertoire.forEach((movie) => foundMovies.push(movie.movieId));
+
+//     const moviesByCinema = [];
+
+//     for (let i = 0; i < foundMovies.length; i += 1) {
+//       const foundMovie = Movie.findOne({
+//         movieId: foundMovies[i],
+//       });
+//       moviesByCinema.push(foundMovie);
+//     }
+
+//     //  foundMovies.forEach(async (movie) => {
+//     //   const foundMovie = await Movie.findOne({
+//     //     movieId: movie,
+//     //   });
+//     //   console.log(foundMovie);
+//     // });
+
+//     console.log(await Promise.all(moviesByCinema));
+//     // const
+//     return null;
+//     // res.json()
+//   } catch (err) {
+//     return res.status(500).send();
+//   }
+// });
+
 module.exports = router;
