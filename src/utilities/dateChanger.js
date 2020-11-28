57c31e0689d1a0770032fe05ed0bdcf42f8a5354
octane\ola <ola_midor@outlook.com>
2020-11-29 00:41:55 +0100

    dateChanger lint fixes

--- src/utilities/dateChanger.js
+++ src/utilities/dateChanger.js
@@ -1,22 +1,28 @@
 const dateChanger = () => {
+  const today = new Date(); // daje pełną datę
+  const weekday = today.getDay(); // daje numer dnia tygodnia
+  const dayMs = 86400000;
+  const week = [];
 
-  const today = new Date() // daje pełną datę
-  const weekday = today.getDay() // daje numer dnia tygodnia
-  const dayMs = 86400000
-  let week = []
-
-  for (let i = 0; i <=6; i++) {
+  for (let i = 0; i <= 6; i += 1) {
     if (i < weekday) {
-      week.push((new Date(Date.parse(today) + (i + 7 - weekday) * dayMs)).toISOString().slice(0, 10))
+      week.push(
+        new Date(Date.parse(today) + (i + 7 - weekday) * dayMs)
+          .toISOString()
+          .slice(0, 10)
+      );
     } else if (i === weekday) {
-      week.push(today.toISOString().slice(0, 10))
+      week.push(today.toISOString().slice(0, 10));
     } else {
-      week.push((new Date(Date.parse(today) + (i - weekday) * dayMs))
-      .toISOString().slice(0, 10))
+      week.push(
+        new Date(Date.parse(today) + (i - weekday) * dayMs)
+          .toISOString()
+          .slice(0, 10)
+      );
     }
   }
   // console.log(week);
-  return week
-} 
+  return week;
+};
 
 module.exports = dateChanger();
