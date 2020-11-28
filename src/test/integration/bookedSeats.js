b492525d0c563527e2e9dddfbbf40e5d0ef2c7d2
octane\ola <ola_midor@outlook.com>
2020-11-28 21:33:53 +0100

    bookedSeats test added

--- src/test/integration/bookedSeats.js
+++ src/test/integration/bookedSeats.js
@@ -6,30 +6,38 @@ const app = require('../../server');
 const { connect } = require('../../config/database');
 const BookedSeat = require('../../models/bookedSeat');
 
+const testData = {
+  cinemaId: '5fbad7ea1b846a4c4c167ea5',
+  movieId: '5fbad7ea1b846a4c4c167eac',
+  date: '2020-12-04',
+  hour: '9:30',
+  bookedSeats: [3, 4],
+  // .replace('_', ':'),
+};
 describe('BookedSeat API test', () => {
   before(async () => {
     await connect();
-    const expectedBooking = new BookedSeat({
-      cinemaId: '5fbad7ea1b846a4c4c167ea5',
-      movieId: '5fbad7ea1b846a4c4c167eac',
-      date: '2020-12-04',
-      hour: '9:30',
-      // .replace('_', ':'),
-    });
+    const expectedBooking = new BookedSeat(testData);
+    await expectedBooking.save();
   });
   after(async () => {
+    await BookedSeat.deleteMany();
     await mongoose.connection.close();
   });
 
   describe('GET /api/v1/bookedSeats/:cinema/:movie/:date/:hour', () => {
     it('should return booked seat', async () => {
       const response = await supertest(app)
-        .get('/api/v1/cinemas')
+        .get(
+          `/api/v1/bookedSeats/${testData.cinemaId}/${testData.movieId}/${
+            testData.date
+          }/${testData.hour.replace(':', '_')}`
+        )
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200);
-      const cinemas = response.body;
-      expect(cinemas).to.have.lengthOf.above(0);
+      const bookedSeat = response.body;
+      expect(bookedSeat.length).to.equal(testData.bookedSeats.length);
     });
   });
 });
