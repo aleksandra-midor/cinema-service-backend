2bcb3d606c8db5fdca1cd812592838b9a5708bde
octane\ola <ola_midor@outlook.com>
2020-11-28 16:43:26 +0100

    ticket email translation added

--- src/mailTemplates/ticket/ticket.js
+++ src/mailTemplates/ticket/ticket.js
@@ -3,9 +3,9 @@ const translationPl = require('./ticketPl.json');
 const translationSv = require('./ticketSv.json');
 
 const email = (ticket) => {
-  let translation = translationSv;
+  let t = translationSv;
   if (ticket.language === 'pl') {
-    translation = translationPl;
+    t = translationPl;
   }
   return {
     From: {
@@ -18,42 +18,40 @@ const email = (ticket) => {
         Name: ticket.customerEmail,
       },
     ],
-    Subject: `Cinema paradiso bilet na ${ticket.movieTitle}`,
-    // Subject: `Ticket/s for the ${ticket.movieTitle} screening`,
-    TextPart: 'My first Mailjet email',
+    Subject: `Cinema Paradiso: ${t.ticketFor} ${ticket.movieTitle}`,
+    TextPart: `Cinema Paradiso: ${t.ticketFor} ${ticket.movieTitle}`,
     HTMLPart: `
-    <html>
-    <body style="background: black; font-size: 120%">
+    <body style="background: black; font-size: 120%; border-bottom: 10px solid #bf2a2a!important">
     <div style="background: #bf2a2a; color: white; padding: 15px; font-size: 18px; font-weight: bold">Cinema Paradiso</div>
-    <div style="max-width: 600px; margin: 40px auto; background: black; color: white; border-bottom: 10px solid #bf2a2a">
-    <h1>${translation.ticketFor} ${ticket.movieTitle}</h1>
-    <p>Cześć, 
-    <br/>to jest twój bilet. 
-    <br>Okaż tego maila przy wejściu na do kina lub wydrukuj i zabierz ze sobą.</p>
+    <div style="max-width: 600px; margin: 40px auto; color: white!important;">
+    <h1>${t.ticketFor} ${ticket.movieTitle}</h1>
+    <p>${t.hi}, 
+    <br/>${t.thisIsYourTicket}. 
+    <br>${t.showItOrPrintIt}</p>
     <br/>
-    <h2>Szczegóły:</h2>
-    <p><small>Film: </small><strong>${ticket.movieTitle}</strong></p>
-    <p><small>Data seansu: </small><strong>${ticket.date}</strong></p>
-    <p><small>Godzina: </small><strong>${ticket.hour}</strong></p>
-    <p><small>Miejsca: </small><strong>${
+    <h2>${t.details}:</h2>
+    <p><small>${t.movie}: </small><strong>${ticket.movieTitle}</strong></p>
+    <p><small>${t.date}: </small><strong>${ticket.date}</strong></p>
+    <p><small>${t.time}: </small><strong>${ticket.hour}</strong></p>
+    <p><small>${t.seats}: </small><strong>${
       ticket.seats.length > 1 ? ticket.seats.join(', ') : ticket.seats[0]
     }</strong>
     </p>
-    <p><small>Kino: </small><strong>${ticket.cinemaName}</strong></p>
+    <p><small>${t.cinema}: </small><strong>${ticket.cinemaName}</strong></p>
     <br/>
-    <h2>Szczegóły płatność:</h2>
-    <p><small>Cena: </small><strong>${ticket.totalPrice} SEK</strong></p>
-    <p><small>Staus płatności: </small><strong>${
+    <h2>${t.paymentDetails}:</h2>
+    <p><small>${t.price}: </small><strong>${ticket.totalPrice} SEK</strong></p>
+    <p><small>${t.paymentStatus}: </small><strong>${
       ticket.paymentStatus
     }</strong></p>
-    <p><small>Identyfikator transakcji:</small></p>
+    <p><small>${t.transactionId}:</small></p>
     <p><strong>${[ticket.paymentId]}</strong></p>
     <br/>
-    <h3 style="margin-top: 20px">Dziękujemy i życzymy udanego seansu.</h3>
+    <h3 style="margin-top: 35px">${t.thankYou}</h3>
+    <p><a href="mailto:cinema@aleksandramidor.com" style="color: white!important;">cinema@aleksandramidor.com</a></p>
+    <br/>
     </div>
     </body>
-    </html>
-    
 `,
     CustomID: 'AppGettingStartedTest',
   };
